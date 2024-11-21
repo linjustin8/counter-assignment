e984e55d85bfcc0467c0cb30a056ec7caa1673a7
justin <justinlin@ufl.edu>
2024-11-20 19:26:15 -0500

    Create counter component tests

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,46 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import Counter from "../components/counter";
 
+let container = null;
 beforeEach(() => {
   // Render the Counter component here
-})
+  container = document.createElement("div");
+  document.body.appendChild(container);
 
-test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
 });
 
-test('should render initial count with value of 0', () => {
+test("renders counter message", () => {
   // Complete the unit test below based on the objective in the line above
+  render(<Counter />, container);
+  const counterMessage = screen.getByText(/Counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
-test('clicking + increments the count', () => {
+test("should render initial count with value of 0", () => {
   // Complete the unit test below based on the objective in the line above
+  render(<Counter />, container);
+  const count = screen.getByText(/0/i);
+  expect(count).toBeInTheDocument();
 });
 
-test('clicking - decrements the count', () => {
+test("clicking + increments the count", () => {
   // Complete the unit test below based on the objective in the line above
+  render(<Counter />, container);
+  const incrementButton = screen.getByText(/\+/i);
+  userEvent.click(incrementButton);
+  const count = screen.getByText(/1/i);
+  expect(count).toBeInTheDocument();
+});
+
+test("clicking - decrements the count", () => {
+  // Complete the unit test below based on the objective in the line above
+  render(<Counter />, container);
+  const decrementButton = screen.getByText(/-/i);
+  userEvent.click(decrementButton);
+  const count = screen.getByText(/-1/i);
+  expect(count).toBeInTheDocument();
 });
